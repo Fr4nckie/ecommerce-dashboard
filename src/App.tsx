846f12dbe1cce85185050adccdf36db806d7f3b6
Franckie <franckie.dev@gmail.com>
2025-11-05 10:32:12 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button.tsx"
+
+function App() {
+  return (
+    <div className="container mx-auto px-4">
+      <h1>Hello</h1>
+      <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default App
