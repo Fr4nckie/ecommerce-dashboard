923044d3a4fedcd80ae93a19d3e3cb1631ba4b28
Franckie <franckie.dev@gmail.com>
2025-11-18 11:15:29 +0300

    feat(router): add Products page route

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom"
 import MainLayout from "./pages/MainLayout.tsx"
 import Dashboard from "./pages/Dashboard.tsx"
 import { kpisLoader } from "./services/kpisLoader.ts"
+import Products from "./pages/Products.tsx"
 
 const router = createBrowserRouter([
   {
@@ -10,7 +11,7 @@ const router = createBrowserRouter([
     children: [
       { index: true, element: <Navigate to="dashboard" replace /> },
       { path: "dashboard", element: <Dashboard />, loader: kpisLoader },
-      { path: "products", element: <div>Products</div> },
+      { path: "products", element: <Products /> },
       { path: "orders", element: <div>orders</div> },
       { path: "customers", element: <div>customers</div> },
       { path: "analytics", element: <div>Analytics</div> },
