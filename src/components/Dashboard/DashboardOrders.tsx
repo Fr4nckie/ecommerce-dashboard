ef034f6dbc33a0eeb35b0fcfb0c5b23865ccf68f
Franckie <franckie.dev@gmail.com>
2025-11-21 10:33:54 +0300

    refactor(orders): make orders table responsive

--- src/components/Dashboard/DashboardOrders.tsx
+++ src/components/Dashboard/DashboardOrders.tsx
@@ -9,6 +9,7 @@ import {
 } from "../ui/table.tsx"
 import OrderRow from "./OrderRow.tsx"
 import type { Order } from "@/types/data.ts"
+import { ChevronsUpDown } from "lucide-react"
 
 const DashboardOrders = () => {
   return (
@@ -22,11 +23,18 @@ const DashboardOrders = () => {
             <TableHeader>
               <TableRow>
                 <TableHead>Product</TableHead>
-                <TableHead>Order ID</TableHead>
-                <TableHead>Customer Name</TableHead>
-                <TableHead>Date</TableHead>
-                <TableHead>Price</TableHead>
-                <TableHead>Status</TableHead>
+                <TableHead className="hidden md:table-cell">Order ID</TableHead>
+                <TableHead className="hidden md:table-cell">
+                  Customer Name
+                </TableHead>
+                <TableHead className="hidden md:table-cell">Date</TableHead>
+                <TableHead className="hidden md:table-cell">Price</TableHead>
+                <TableHead className="text-right md:text-left">Status</TableHead>
+                <TableHead className="md:hidden">
+                  <div className="flex justify-end">
+                    <ChevronsUpDown size={18} />
+                  </div>
+                </TableHead>
               </TableRow>
             </TableHeader>
             <TableBody>
