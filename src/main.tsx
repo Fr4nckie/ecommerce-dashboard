cd12ebb4f6926ff456645627bd2b12c8e6afe1d0
Franckie <franckie.dev@gmail.com>
2025-11-25 11:18:46 +0300

    feat(customers-rtk): configure store and create customersApi service

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import { createRoot } from "react-dom/client"
 import "./index.css"
 import App from "./App.tsx"
 import { ThemeProvider } from "./context/ThemeProvider.tsx"
+import { Provider } from "react-redux"
+import { store } from "./app/store.ts"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </ThemeProvider>
   </StrictMode>
 )
