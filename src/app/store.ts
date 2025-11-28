16446cff3613d708bc22bb46f3e60dec8b8a8bd5
Franckie <franckie.dev@gmail.com>
2025-11-28 13:18:21 +0300

    refactor(store): add productsApi and ordersApi configurations

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,12 +1,19 @@
 import { customersApi } from "@/features/customers/customersApi.ts"
+import { ordersApi } from "@/features/orders/ordersApi.ts"
+import { productsApi } from "@/features/products/productsApi.ts"
 import { configureStore } from "@reduxjs/toolkit"
 
 export const store = configureStore({
   reducer: {
     [customersApi.reducerPath]: customersApi.reducer,
+    [productsApi.reducerPath]: productsApi.reducer,
+    [ordersApi.reducerPath]: ordersApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(customersApi.middleware),
+    getDefaultMiddleware()
+      .concat(customersApi.middleware)
+      .concat(productsApi.middleware)
+      .concat(ordersApi.middleware),
 })
 
 export type RootState = ReturnType<typeof store.getState>
